00d22f682ab1c78fa503f30b47667b27a1611bf2
wills blake <richwblake@gmail.com>
2020-10-08 22:10:33 -0400

    Styled login logout buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
+  include Pundit
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: [session[:user_id]])
