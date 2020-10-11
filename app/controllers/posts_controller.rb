32a4a92aeafe7a1481aab8c59a98748b7f053e39
wills blake <richwblake@gmail.com>
2020-10-11 01:15:39 -0400

    Styled post creation form. Added summary to post params in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :summary)
   end
 end
