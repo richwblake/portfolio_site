590a201a1f694f2eef730fc43f8bb547893a8b97
wills blake <richwblake@gmail.com>
2020-10-10 21:06:22 -0400

    Added summary to posts model. Added styling to posts index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_025619) do
+ActiveRecord::Schema.define(version: 2020_10_11_003357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_10_09_025619) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.string "summary"
   end
 
   create_table "users", force: :cascade do |t|
