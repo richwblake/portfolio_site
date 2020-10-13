cc7f838fab78f21b89b9d84605c6c43d0f8c2637
wills blake <richwblake@gmail.com>
2020-10-12 21:39:55 -0400

    Added timestamps to posts. Adjusted post show css

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,11 @@ class Post < ApplicationRecord
   has_many :users, through: :comments
 
   validates :title, presence: true
+  validates :summary, presence: true
   validates :content, presence: true
+
+  def get_formatted_date_and_time
+    created = self.created_at.in_time_zone('Eastern Time (US & Canada)')
+    "#{created.strftime("%m/%d/%y")} at #{created.strftime("%I:%M%p")}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_213225) do
+ActiveRecord::Schema.define(version: 2020_10_13_004429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_10_11_213225) do
     t.string "title"
     t.text "content"
     t.string "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
